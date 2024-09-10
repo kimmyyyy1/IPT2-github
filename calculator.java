89b0dde66431c47fd05169e417d6d7dfa493f15d
Elisha <elishadale@gmail.com>
2024-09-10 11:37:58 +0800

    modulus

--- calculator.java
+++ calculator.java
@@ -20,6 +20,10 @@ public class Calculator{
             throw new ArithmeticException("Division by zero");
         }
     }
+
+    public static int modulus(int a, int b){
+        return a % b;
+    }
     
 
                 public static void main(String[]args){
@@ -27,6 +31,7 @@ public class Calculator{
                     System.out.println("Substraction: " + subtraction(5,3));
                     System.out.println("Multiplicaiton: " + multiplication(5,3));
                     System.out.println("Division: " + division(5,3));
+                    System.out.printIn("Modulus: " + modulus(5,3));
                     
                 }
             }
