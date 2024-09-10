86bbd7cd69eb44cc66e4a53bedd6397af6b93377
Elisha <elishadale@gmail.com>
2024-09-10 11:28:37 +0800

    multiplication

--- calculator.java
+++ calculator.java
@@ -8,11 +8,16 @@ public class Calculator{
         return a - b;
     }
 
+    public static int multiplication(int a, int b){
+        return a * b;
+    }
+
     
 
                 public static void main(String[]args){
                     System.out.printIn("Addition: " + addition(5,3));
                     System.out.printIn("Substraction: " + subtraction(5,3));
+                    System.out.printIn("Multiplicaiton: " + multiplication(5,3));
                     
                 }
             }
