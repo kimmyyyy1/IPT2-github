0d0bc6b51de25f2ca85a4bdbfa53a258b0f760dd
Elisha <elishadale@gmail.com>
2024-09-10 11:21:44 +0800

    addition

---
+++ calculator.java
@@ -0,0 +1,13 @@
+public class Calculator{
+    
+    public static int addition(int a, int b){
+        return a + b;
+    }
+
+    
+
+                public static void main(String[]args){
+                    System.out.printIn("Addition: " + addition(5,3));
+                    
+                }
+            }
