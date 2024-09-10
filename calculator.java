255fae0455e1719fd1a0c51cbfec9ad2292b42a9
Elisha <elishadale@gmail.com>
2024-09-10 11:26:19 +0800

    subtraction

--- calculator.java
+++ calculator.java
@@ -4,10 +4,15 @@ public class Calculator{
         return a + b;
     }
 
+    public static int subtraction(int a, int b){
+        return a - b;
+    }
+
     
 
                 public static void main(String[]args){
                     System.out.printIn("Addition: " + addition(5,3));
+                    System.out.printIn("Substraction: " + subtraction(5,3));
                     
                 }
             }
